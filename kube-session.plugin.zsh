9f58a19243449cca1f34c1faed1554e6e49af193
ANGkeith <angkeith@hotmail.sg>
2023-12-24 15:11:10 +0800

    feat: add p10k integration

--- kube-session.plugin.zsh
+++ kube-session.plugin.zsh
@@ -2,6 +2,9 @@ KUBECONFIG=${KUBECONFIG:-$HOME/.kube/config}
 KUBECONFIG_SESSION_DIR="${XDG_STATE_HOME:-$HOME/.kube}/kubech"
 KUBE_SESSION_CONFIG="$KUBECONFIG_SESSION_DIR/$$-config"
 
+KUBE_SESSION_POWERLEVEL9K_FOREGROUND=${KUBE_SESSION_POWERLEVEL9K_FOREGROUND:-13}
+KUBE_SESSION_POWERLEVEL9K_CONTENT_EXPANSION=${KUBE_SESSION_POWERLEVEL9K_CONTENT_EXPANSION:-" (session)"}
+
 _init_kubech() {
   if [[ ! -d $KUBECONFIG_SESSION_DIR ]]; then
     mkdir -p "$KUBECONFIG_SESSION_DIR"
@@ -14,6 +17,13 @@ _init_kubech() {
   unset -f _init_kubech
 } && _init_kubech
 
+_activate_kube-session_p10k_prompt() {
+  typeset -g POWERLEVEL9K_KUBECONTEXT_KUBE_SESSION_FOREGROUND="$KUBE_SESSION_POWERLEVEL9K_FOREGROUND"
+  typeset -g POWERLEVEL9K_KUBECONTEXT_CLASSES=('*' KUBE_SESSION)
+  POWERLEVEL9K_KUBECONTEXT_KUBE_SESSION_CONTENT_EXPANSION="${POWERLEVEL9K_KUBECONTEXT_DEFAULT_CONTENT_EXPANSION}${KUBE_SESSION_POWERLEVEL9K_CONTENT_EXPANSION}"
+  p10k reload
+}
+
 _kube-session_activate() {
   if [[ -f $KUBECONFIG ]]; then
     cp -n "$KUBECONFIG" "$KUBE_SESSION_CONFIG" 2>/dev/null
@@ -25,6 +35,7 @@ _kube-session_activate() {
     }
 
     export KUBECONFIG="$KUBE_SESSION_CONFIG"
+    if command -v p10k &>/dev/null; then _activate_kube-session_p10k_prompt; fi
   else
     cat <<EOF >/dev/stderr
 error: \`$KUBECONFIG\` does not exists
